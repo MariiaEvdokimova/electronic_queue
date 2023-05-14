24c7ab347e34334ddc52eb95f8909420092e3d2d
Mariia Evdokimova <evdokimovamariam@gmail.com>
2023-05-14 22:19:56 +0300

    update

--- stage_3_1.py
+++ stage_3_1.py
@@ -8,52 +8,51 @@
 - Перед приглашением пользователю ввести фамилию, показать длину очереди перед ним: сколько человек перед ним."""
 
 
-###
+from datetime import datetime
 def main():
-    spec = {'idc_width': 3, 'surname_width': 15, 'code_width': 3}
+    spec = {'idc_width': 3, 'surname_width': 15, 'code_width': 3, 'date_width': 23, }
     idc = 1
     eq = {}
-    version = "2.2"
+    version = "3.1"
     codes = "MNPS"
     while True:
-      eq_print(eq, version, spec)
-      data = input("Введите фамилию или пустая строка для выхода: ")
-      if data == "":
-         print("Всего вам доброго! До свидания!")
-         break
-      while True:
-          code = input(f"Введите код операции. Допустимые коды: {codes}: ")
-          if is_code_valid(code, codes):
-              break
-          else:
-           print("Неправильный код операции.")
-    eq_add_client(eq, idc, data, code)
-    idc += 1
-
-#def is_code_valid(code: str, codes: str) -> bool:
-#   if code.upper() in codes:
-#        return True
-#   else:
-#        return False
+        eq_print(eq, version, spec)
+        print(f"Перед Вами {len(eq)} человек")
+        data = input("Введите фамилию, или пустая строка для выхода: ")
+        if data == "":
+           print("Всего вам доброго! До свидания!")
+           break
+        while True:
+           code = input(f"Введите код операции. Допустимые коды: {codes}: ")
+           if is_code_valid(code, codes):
+               break
+           else:
+               print("Неверный код, попробуйте еще раз")
+        eq_add_client(eq, idc, data, code)
+        idc += 1
 
 def is_code_valid(code: str, codes: str) -> bool:
     return code.upper() in codes
 
 def eq_add_client(eq: dict, idc: int, surname: str, code: str) -> list:
-    eq[idc] = {'surname': surname, 'code': code.upper()}
+    eq[idc] = {'surname': surname, 'code': code.upper(), 'date': datetime.now()}
     return eq
 def idc_str(idc:int) -> str:
     return str(idc).zfill(3)
 
 def table_row_str(idc: int, client: dict, spec: dict) -> str:
+    date = client['date'].strftime("%d-%m-%y %H:%M:%S")
+    surname = client['surname'] if len(client['surname']) <=10 else client['surname'][:10] + "..."
     result = f"|{idc_str(idc):^{spec['idc_width']}s}|" + \
-             f"{client['surname']:^{spec['surname_width']}s}|" + \
-             f"{client['code']:^{spec['code_width']}s}|"
+              f"{surname:^{spec['surname_width']}s}|" + \
+              f"{client['code']:^{spec['code_width']}s}|" + \
+              f"{date:^{spec['date_width']}s}|"
     return result
 
 def eq_print(eq: dict, version: str, spec: dict) -> None:
     print(f"Электронная очередь. Версия {version}")
-    total_width = spec['idc_width'] + spec['surname_width'] + spec['code_width'] + 4
+    total_width = spec['idc_width'] + spec['surname_width'] + spec['code_width'] + \
+                  spec['date_width'] + 5
     print("-" * total_width)
     for idc, client in eq.items(): #{'surname': ххх, 'code': ххх}
         print(table_row_str(idc, client, spec))
